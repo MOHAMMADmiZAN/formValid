36e6b776da47ffca19fc94b3a50d5320429e10eb
MOHAMMAD MIZAN <mohammad25524143@gmail.com>
2021-01-17 00:19:38 +0600

    formValidation with Php

---
+++ response.php
@@ -0,0 +1,49 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $confirmPassword = $_POST["confirmPassword"];
+    $gender = $_POST["gender"];
+    $submit = $_POST["submit"];
+    $nameRegex = !preg_match("/^([a-zA-Z' ]+)$/i", $name);
+    $emailRegex = !preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,10})$/", $email);
+    $passwordRegex = !preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,100}$/", $password);
+    //name Validation //
+    if (isset($name) && $nameRegex) {
+        $errorMessage = "<p style='color:red;'>Only alphabets and whitespace are allowed.</p>";
+        echo $errorMessage;
+    } else {
+        echo "</br>";
+        echo $name;
+    }
+    // email validation //
+    if (isset($email) && $emailRegex && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errorMessage = "<p style='color:red;'>Only Valid Mail</p>";
+        echo $errorMessage;
+    } else {
+        echo "</br>";
+        echo $email;
+    }
+    // password Validation //
+
+    if (isset($password) && $passwordRegex) {
+        $errorMessage = "<p style='color:red;'>its Not Valid</p>";
+        echo $errorMessage;
+
+    } else {
+        echo "</br>";
+        echo $password;
+    }
+    // confirmPassword valid //
+    if (isset($confirmPassword) && $password !== $confirmPassword) {
+        $errorMessage = "<p style='color:red;'>its Not same</p>";
+        echo $errorMessage;
+    } else {
+        echo "Its Awesome";
+    }
+
+
+} else {
+    header("Location:index.php");
+}
