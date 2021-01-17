481044530d04ce30a1c07619f2006f019ac2ed23
MOHAMMAD MIZAN <mohammad25524143@gmail.com>
2021-01-17 15:14:32 +0600

    project reset

--- response.php
+++ response.php
@@ -8,9 +8,12 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
     $submit = $_POST["submit"];
     $nameRegex = !preg_match("/^([a-zA-Z' ]+)$/i", $name);
     $emailRegex = !preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,10})$/", $email);
-    $passwordRegex = !preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,100}$/", $password);
+    $passwordRegex = !preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,100}$/", $password);
     //name Validation //
-    if (isset($name) && $nameRegex) {
+    if (empty($name)) {
+        $errorMessage = "<p style='color:red;'>please say something</p>";
+        echo $errorMessage;
+    } else if ($nameRegex) {
         $errorMessage = "<p style='color:red;'>Only alphabets and whitespace are allowed.</p>";
         echo $errorMessage;
     } else {
@@ -18,7 +21,10 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
         echo $name;
     }
     // email validation //
-    if (isset($email) && $emailRegex && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if (empty($email)) {
+        $errorMessage = "<p style='color:red;'>please say something</p>";
+        echo $errorMessage;
+    } elseif ($emailRegex) {
         $errorMessage = "<p style='color:red;'>Only Valid Mail</p>";
         echo $errorMessage;
     } else {
@@ -26,8 +32,10 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
         echo $email;
     }
     // password Validation //
-
-    if (isset($password) && $passwordRegex) {
+    if (empty($password)) {
+        $errorMessage = "<p style='color:red;'>please say something</p>";
+        echo $errorMessage;
+    } elseif ($passwordRegex) {
         $errorMessage = "<p style='color:red;'>its Not Valid</p>";
         echo $errorMessage;
 
@@ -36,10 +44,17 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
         echo $password;
     }
     // confirmPassword valid //
-    if (isset($confirmPassword) && $password !== $confirmPassword) {
-        $errorMessage = "<p style='color:red;'>its Not same</p>";
+    if (empty($confirmPassword)) {
+        $errorMessage = "<p style='color:red;'>its empty</p>";
+        echo $errorMessage;
+    } elseif ($password !== $confirmPassword) {
+        $errorMessage = "<p style='color:red;'>its not same</p>";
+        echo $errorMessage;
+    } elseif ($password === $confirmPassword && $passwordRegex) {
+        $errorMessage = "<p style='color:red;'>its Not Valid</p>";
         echo $errorMessage;
     } else {
+        echo "</br>";
         echo "Its Awesome";
     }
 
