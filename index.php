e8e0e2efb042d0dba74e64b15f2f0600d1bef963
MOHAMMAD MIZAN <mohammad25524143@gmail.com>
2021-01-23 17:01:01 +0600

    solve[session]

--- index.php
+++ index.php
@@ -14,11 +14,11 @@ session_start();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css" media="all">
-        <style>
-            .errorMessageInput {
-                border: 2px solid red;
-            }
-        </style>
+    <!--        <style>-->
+    <!--            .errorMessageInput {-->
+    <!--                border: 2px solid red;-->
+    <!--            }-->
+    <!--        </style>-->
 </head>
 <body>
 <header class="formTitle">
@@ -30,8 +30,8 @@ session_start();
             <div class="col-lg-12">
                 <form action="response.php" method="post">
                     <label for="name">fullName:</label>
-                    <input type="text" id="name" placeholder="Type Your Name"
-                           name="name" <?php if (isset($_SESSION["errorMessageName"]) || isset($_SESSION["errorMessageRegexName"])) print("class='errorMessageInput'") ?>
+                    <input type="text" id="name" placeholder="Type Your Name" class="errorMessageInput"
+                           name="name"
                     >
                     <?php
                     if (isset($_SESSION["errorMessageName"])) {
