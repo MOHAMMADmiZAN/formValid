b0ee123a74350956943497e1b4fe541380382100
MOHAMMAD MIZAN <mohammad25524143@gmail.com>
2021-01-23 16:52:39 +0600

    solve[session]

--- index.php
+++ index.php
@@ -13,12 +13,12 @@ session_start();
     <title>Form</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <style>
-        .errorMessageInput {
-            border: 2px solid red;
-        }
-    </style>
+    <link rel="stylesheet" href="css/style.css" media="all">
+        <style>
+            .errorMessageInput {
+                border: 2px solid red;
+            }
+        </style>
 </head>
 <body>
 <header class="formTitle">
