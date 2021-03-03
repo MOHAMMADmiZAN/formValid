81df6d0338bed4e445d48eb69ba25fb7d623f07f
MOHAMMAD MIZAN <mohammad25524143@gmail.com>
2021-03-04 00:50:01 +0600

    solve[database user delete && update status]

--- userDelete.php
+++ userDelete.php
@@ -8,51 +8,54 @@ if (isset($dataBase)) {
     if (isset($id)) {
         // update query //
         $update = "UPDATE `users` SET `status` = 2 WHERE `id` = '$id'";
-    }
-    if (isset($update)) {
-        $updateQuery = $dataBase->query($update);
-        $dataBase->close();
-    }
-    if (isset($updateQuery)) {
-        if ($updateQuery) {
-            $_SESSION['recoverMsg'] = "IF YOU RECOVER THIS USER PLEASE VISIT <a href='recoverUser.php' target='_blank' class='btn btn-info mx-3'>RECOVER USER</a>";
-            header("Location:userList.php");
-        } else {
-            echo "UPDATE NOT WORKING";
+        if (isset($update)) {
+            $updateQuery = $dataBase->query($update);
+            $dataBase->close();
+            if (isset($updateQuery)) {
+                if ($updateQuery) {
+                    $_SESSION['recoverMsg'] = "IF YOU RECOVER THIS USER PLEASE VISIT <a href='recoverUser.php' target='_blank' class='btn btn-info mx-3'>RECOVER USER</a>";
+                    header("Location:userList.php");
+                } else {
+                    echo "UPDATE NOT WORKING";
+                }
+            }
         }
+
     }
+    /// recover query //
     if (isset($recoverId)) {
         // recover query //
         $recover = "UPDATE `users` SET `status` = 1 WHERE `id` = '$recoverId'";
-    }
-    if (isset($recover)) {
-        $recoverQuery = $dataBase->query($recover);
-        $dataBase->close();
-    }
-    if (isset($recoverQuery)) {
-        if ($recoverQuery) {
-            header("Location:recoverUser.php");
-        } else {
-            echo "RECOVER NOT WORKING";
+        if (isset($recover)) {
+            $recoverQuery = $dataBase->query($recover);
+            $dataBase->close();
+            if (isset($recoverQuery)) {
+                if ($recoverQuery) {
+                    header("Location:recoverUser.php");
+                } else {
+                    echo "RECOVER NOT WORKING";
+                }
+            }
         }
     }
-
-
+    /// Permanent Delete Query ///
     if (isset($deletedId)) {
         /// delete query ///
         $delete = "DELETE FROM `users` WHERE `id` = '$deletedId'";
-    }
-    if (isset($delete)) {
-        $deleteQuery = $dataBase->query($delete);
-        $dataBase->close();
+        if (isset($delete)) {
+            $deleteQuery = $dataBase->query($delete);
+            $dataBase->close();
+            if (isset($deleteQuery)) {
+                if ($deleteQuery) {
+                    header("Location:recoverUser.php");
+                } else {
+                    echo "DELETE NOT WORKING";
+                }
+            }
 
-    }
-    if (isset($deleteQuery)) {
-        if ($deleteQuery) {
-            header("Location:recoverUser.php");
-        } else {
-            echo "DELETE NOT WORKING";
         }
     }
+
+
 }
 
