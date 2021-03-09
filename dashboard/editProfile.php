eb5d410024da31f76047398cc779a6bec2b4316b
MOHAMMAD MIZAN <mohammad25524143@gmail.com>
2021-03-09 14:51:47 +0600

    create login page verify login password

--- dashboard/editProfile.php
+++ dashboard/editProfile.php
@@ -3,9 +3,9 @@
 
 <div class="sl-mainpanel">
     <div class="sl-pagebody">
-   <div class="alert alert-primary text-center">
-       <h1>This is a Edit Page</h1>
-   </div>
+        <div class="alert alert-primary text-center">
+            <h1>This is a Edit Page</h1>
+        </div>
         <?php if (isset($_SESSION['update'])) { ?>
             <div class="alert alert-success text-center">
                 <?php echo $_SESSION['update'];
@@ -14,23 +14,41 @@
             </div>
         <?php }
         ?>
-      <div class="row">
-          <div class="col-lg-5  m-auto">
-              <form action="editProfileResponse.php" method="post" enctype="multipart/form-data">
-                  <div class="form-group">
-                      <label for="editName"></label>
-                      <input type="text" id="editName" class="form-control" name="name" placeholder="Update Your Full Name">
-                      <label for="editEmail"></label>
-                      <input type="text" id="editEmail" class="form-control" name="email" placeholder="Update Your email">
-                      <label for="editPhone"></label>
-                      <input type="text" id="editPhone" class="form-control" name="phone" placeholder="Update Your Phone Number">
-                      <label for="editImage"></label>
-                      <input type="file" id="editImage" class="form-control" name="image"  placeholder="Update Your Profile Image">
-                      <button type="submit" class="btn btn-primary my-3 p-3 b-ra-5 mx-auto d-block" name="update">Update</button>
-                  </div>
-              </form>
-          </div>
-      </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6  m-auto">
+                    <form action="editProfileResponse.php" method="post" enctype="multipart/form-data">
+                        <div class="form-group">
+                            <label for="editName"></label>
+                            <input type="text" id="editName" class="form-control" name="name"
+                                   placeholder="Update Your Full Name">
+                            <?php
+                            if (isset($_SESSION['EmailError2'])) {
+                                ?>
+                                <div class="alert alert-danger text-center text-uppercase">
+                                    <?php echo $_SESSION['EmailError2'];
+                                    unset($_SESSION['EmailError2']);
+                                    ?>
+                                </div>
+                            <?php } ?>
+                            <label for="editEmail"></label>
+                            <input type="text" id="editEmail" class="form-control" name="email"
+                                   placeholder="Update Your email">
+                            <label for="editPhone"></label>
+                            <input type="text" id="editPhone" class="form-control" name="phone"
+                                   placeholder="Update Your Phone Number">
+                            <label for="editImage"></label>
+                            <input type="file" id="editImage" class="form-control" name="image"
+                                   placeholder="Update Your Profile Image">
+                            <button type="submit" class="btn btn-primary my-3 p-3 b-ra-5 mx-auto d-block" name="update">
+                                Update
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+        </div>
     </div><!-- sl-pagebody -->
 </div><!-- sl-mainpanel -->
 
--- dashboard/editProfileResponse.php
+++ dashboard/editProfileResponse.php
@@ -7,17 +7,33 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
     $email = $_POST['email'];
     $phone = $_POST['phone'];
 //    $img = $_FILES['img'];
+    $duplicateCheck = "SELECT COUNT(*) as emailverified,fullName,`cellNumber` FROM `users` WHERE `email`='$email'";
     $editProfile = " UPDATE `users` SET `fullName`='$name',`email`='$email',`cellNumber`='$phone' WHERE `id`='$sessionId'";
+
     if (isset($dataBase)) {
-        $editProfileQuery = $dataBase->query($editProfile);
-        $dataBase->close();
-        if ($editProfileQuery) {
-            $_SESSION['update'] = "Profile successfully update";
-            header("Location:editProfile.php");
+        if (isset($duplicateCheck)) {
+            $duplicateCheckQuery = $dataBase->query($duplicateCheck);
+            if (isset($duplicateCheckQuery)) {
+                $duplicateCheckAssoc = $duplicateCheckQuery->fetch_assoc();
+                if (isset($duplicateCheckAssoc)) {
+
+                    if ($duplicateCheckAssoc['emailverified'] > 0 === true && empty($email)) {
+                        $_SESSION['EmailError2'] = 'Try With Different Email !!';
+                        header("Location:editProfile.php");
+                    } else {
+                        $editProfileQuery = $dataBase->query($editProfile);
+                        $dataBase->close();
+                        if ($editProfileQuery) {
+                            $_SESSION['update'] = "Profile successfully update";
+                            header("Location:editProfile.php");
 
 
-        } else {
-            echo "query Fail";
+                        } else {
+                            echo "query Fail";
+                        }
+                    }
+                }
+            }
         }
 
     } else {
